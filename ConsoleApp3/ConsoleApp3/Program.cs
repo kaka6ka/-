8fd86ff1cbe26cb79c1044c4b7a47fa8cf7d2289
sokolov <133795769+kaka6ka@users.noreply.github.com>
2024-04-11 21:27:22 +0400

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass Point = new MyClass();
+            
+                 
+        }
+        public class MyClass
+        {
+            public MyClass()
+            {
+                Console.WriteLine("insert number 1");
+                x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите число 2");
+                y = Convert.ToInt32(Console.ReadLine());
+                z = x + y;
+                Console.WriteLine("Ответ:");
+                Console.WriteLine(z);
+            }
+            public int x;
+            public int y;
+            public int z;
+           
+                }
+            
+        }
+        
+    }
+
